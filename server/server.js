6d8f2130784b5fa03205b68e743189d43ac0313c
Tushar Aggarwal <tushara272@gmail.com>
2021-11-09 15:13:06 +0530

    added connection and disconnection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
-;const path =require('path');
+const path =require('path');
 const express=require('express');
 const app=express();
-
-
+const port=process.env.PORT||3000;
+const socketIO=require('socket.io');
+const http=require('http');
+const server=http.createServer(app);
+const io=socketIO(server);
 const publicpath=(path.join(__dirname,'/../public'));
 app.use(express.static(publicpath));
 
-app.listen('3000',()=>{
-    console.log("Server is running on port 3000")
-})
+
+io.on('connection',(socket)=>{
+    // console.log(io);
+    console.log("A new user just connected");
+    socket.on('disconnect',(socket)=>{
+        // console.log(io);
+        console.log("A new user just disconnected");
+    });
+});
+
+server.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+}) 
